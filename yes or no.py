158c205b704eda2fb56c0316fedb9b676699e820
subashbabu97 <48273911+subashbabu97@users.noreply.github.com>
2019-06-06 15:43:56 +0530

    Update yes or no.py

--- yes or no.py
+++ yes or no.py
@@ -1,9 +1,10 @@
 p=int(input())
-a=0
-for x in range(1,p+1):
+a=p//2
+b=0
+for x in range(2,a+1):
         if(p%x==0):
-                a=a+1
-if d==2:
+                b=1
+if b==0:
         print("yes")
 else:
         print("no")
